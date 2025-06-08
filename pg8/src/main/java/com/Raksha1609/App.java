963d917fe18f443c85a03d667595425c210fb93d
Raksha1609 <rakshamahadev16@gmail.com>
2025-06-08 23:54:27 +0530

    first commit

---
+++ pg8/src/main/java/com/Raksha1609/App.java
@@ -0,0 +1,13 @@
+package com.Raksha1609;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
